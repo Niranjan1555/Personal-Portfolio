62a1f4790145f9ba91cd4b5bda35580f9bf16988
Niranjan1555 <puthineediniranjan@gmail.com>
2025-12-15 15:31:22 +0530

    Add files via upload

---
+++ e1.py
@@ -0,0 +1,72 @@
+#a = 10
+#b = 14
+#print("Sum:", a+b)
+#print("Difference:", a-b)
+#print("Product:", a*b)
+#print("Quotient:", a/b)
+
+
+#a = 10
+#b = 6
+#if a > b:
+#    print("A is Postive")
+#else:
+ #   print("B is Negative")
+
+#a = 5
+#b = 6
+#c = 5
+#d = 7
+#e = 7
+#f = 8
+#print(int(a))
+#print(int(b))
+#print(float(c))
+#print(str(d))
+#print(int(e))
+#print(str(f))
+
+
+# int example
+a =10
+b = -9
+print(a,b)
+
+# variable example
+Name = 'Niranjan'
+Age = 19
+Height = 6.0
+is_student = True
+
+print(Name)
+print(Age)
+print(Height)
+print(is_student)
+
+#float
+
+a = 15.0
+b = 20.0
+print(a,b)
+
+#string
+
+city = "Secunderbad"
+District = "Hyderabad"
+print(city)
+print(District)
+
+#Boolean
+Secunderbad_is_captial_of_Hyderabad = True
+Hyderabad_is_captial_of_secunderbad = False
+print(Secunderbad_is_captial_of_Hyderabad)
+print(Hyderabad_is_captial_of_secunderbad) 
+
+#List
+numbers = [1,2,3,4,5,6,7,8]
+print(numbers)
+
+Fruits = ["Niranjan", "Ranjith","Bharath"]
+print(Fruits)
+print(Fruits[0])
+
